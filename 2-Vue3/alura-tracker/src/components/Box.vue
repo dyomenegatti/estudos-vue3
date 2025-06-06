a6413c32eec95b05387c1d329fff3f7b5b7a79ca
Dyovana <dyo.menegatti@hotmail.com>
2025-06-06 16:31:24 -0300

    finalizar curso vue3 conhecendo o framework

---
+++ 2-Vue3/alura-tracker/src/components/Box.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'BoxApp',
+})
+</script>
+
+<style scoped>
+.box {
+  background: #1f1f1f;
+}
+</style>
---
+++ 2-Vue3/alura-tracker/src/components/Tarefa.vue
@@ -0,0 +1,29 @@
+<template>
+  <Box>
+    <div class="columns">
+      <div class="colum is-7">{{ tarefa.descricao || 'Tarefa sem descrição' }}</div>
+
+      <div class="column">
+        <cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"></cronometro>
+      </div>
+    </div>
+  </Box>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import Cronometro from './Cronometro.vue';
+import ITarefa from '../interfaces/ITarefa';
+import Box from './Box.vue';
+
+export default defineComponent({
+  components: { Cronometro, Box },
+  name: 'TarefaApp',
+  props: {
+    tarefa: {
+      type: Object as PropType<ITarefa>,
+      required: true
+    }
+  }
+})
+</script>
